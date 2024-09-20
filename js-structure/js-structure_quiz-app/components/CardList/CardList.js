8a52012c0f2d7d4f4178595c4f9423d9f36f48ac
Andreas Kopp <andreas.kopp90@gmail.com>
2024-09-20 15:55:45 +0200

    challenge solved

---
+++ js-structure/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,15 @@
+import { cards } from "../../lib/data.js";
+import Card from "../Card/Card.js";
+
+cards;
+export default function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
