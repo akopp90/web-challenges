5a90db8bfb0b22b238ff904cfec240e5eb728402
Andreas Kopp <andreas.kopp90@gmail.com>
2024-10-02 11:36:20 +0200

    challenge solved

---
+++ react-state-1/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./FavoriteButton.css";
+import StarFilled from "./star-filled.svg?react";
+import Star from "./star.svg?react";
+
+export default function FavoriteButton() {
+  // this should be a state variable
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function handleClick() {
+    setIsFavorite(true);
+  }
+  return (
+    <button
+      className="favorite-button"
+      onClick={() => {
+        console.log("favorite button clicked");
+        handleClick();
+      }}
+      aria-label="favorite"
+    >
+      {isFavorite ? <StarFilled /> : <Star />}
+    </button>
+  );
+}
